ea7a24f65ad39be5c40c5ca0c2069ecc3f5ca8d1
MD. RAHAMAT ULLAH <urahamat01@gmail.com>
2019-03-25 06:02:16 -0700

    sdijsd

--- eclips-class/AirlineManagement/DaoImple/SalesDaoImp.java
+++ eclips-class/AirlineManagement/DaoImple/SalesDaoImp.java
@@ -2,7 +2,9 @@ package DaoImple;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import DAO.ProductDao;
@@ -12,58 +14,82 @@ import Pojo.Product;
 public class SalesDaoImp implements ProductDao {
 
 	Connection conn = DBConnection.getConnection();
-	public static void main (String[]agrs ) 
-	{
-		
+
+	public static void main(String[] agrs) {
+
 		SalesDaoImp obj = new SalesDaoImp();
 		obj.createTable();
 	}
-	
+
 	@Override
 	public void createTable() {
 
-		
-			String sql ="create table if not Exists sales(id int(4) auto_increment primary key,product_name varchar(20),product_code varchar(20),sales_date date,quantity int(20),unit_price double,total_amount double)";
-			try {
-				PreparedStatement ps = conn.prepareStatement(sql);
-				ps.execute();
-				System.out.println("table create");
-			
-			} catch (Exception e) {
-			
-				
-				
-				e.printStackTrace();
-			}
-			
-			
-			
+		String sql = "create table if not Exists sales(id int(4) auto_increment primary key,product_name varchar(20),product_code varchar(20),sales_date date,quantity int(20),unit_price double,total_amount double)";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.execute();
+			System.out.println("table create");
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void save(Product p) {
-		String sql ="insert into sales (product_name, product _code, sales_date, quantity, unit_price, total_amount) values(?,?,?,?,?,?)";
+		String sql = "insert into sales (product_name, product _code, sales_date, quantity, unit_price, total_amount) values(?,?,?,?,?,?)";
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setString(parameterIndex, x);
-		
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			ps.setString(1, p.getProductName());
+			ps.setString(2, p.getProductCode());
+			ps.setDate(3, p.getDate());
+			ps.setInt(4, p.getQty());
+			ps.setDouble(5, p.getuPrice());
+			ps.setDouble(6, p.getTotalAmount());
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	@Override
 	public void update(Product p) {
-		// TODO Auto-generated method stub
-		
+		String sql = "update sales set product_name=?,product_code=?, sales_date=?,quantity=?,unit_price=?,total_amount=? where id=?";
+
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, p.getProductName());
+			ps.setString(2, p.getProductCode());
+			ps.setDate(3, p.getDate());
+			ps.setInt(4, p.getQty());
+			ps.setDouble(5, p.getuPrice());
+			ps.setDouble(6, p.getTotalAmount());
+			ps.setInt(7, p.getId());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void delete(int id) {
-		// TODO Auto-generated method stub
-		
+		String sql = "delete from sales where id =?";
+
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ps.executeUpdate();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -80,8 +106,24 @@ public class SalesDaoImp implements ProductDao {
 
 	@Override
 	public List<Product> getProducts() {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Product> list = new ArrayList<Product>();
+		String sql = "select * from sales";
+
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				Product p = new Product(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getInt(5),
+						rs.getDouble(6), rs.getDouble(7));
+				list.add(p);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
 	}
 
 }
